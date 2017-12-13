b7f4c5c90abaac6cb9cdcf777eb25bed266cf189
xinli <l.xin@yagro.cn>
2017-12-13 13:21:40 +0800

    Modify stm32f4xx_conf.h

--- User/stm32f4xx_conf.h
+++ User/stm32f4xx_conf.h
@@ -161,10 +161,10 @@ extern "C" {
   *               If expr is true, it returns no value.
   * @return None.
   */
-#define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
-void assert_failed(uint8_t* file, uint32_t line);
+#define assert_param(expr)  ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
+void assert_failed(uint8_t *file, uint32_t line);
 #else
-#define assert_param(expr) ((void)0)
+#define assert_param(expr)  ((void)0)
 #endif /* USE_FULL_ASSERT */
 
 /* Function definitions ------------------------------------------------------*/
